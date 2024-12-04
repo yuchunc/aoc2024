a89a9e49699b4b90a08f4771de3a040a1d7ced73
Mickey Chen <hey.mickabo@gmail.com>
2024-12-04 15:42:40 +0800

    finished day4

--- lib/aoc2024.ex
+++ lib/aoc2024.ex
@@ -1,6 +1,6 @@
-defmodule Aoc2024 do
+defmodule AoC2024 do
   @moduledoc """
-  Documentation for `Aoc2024`.
+  Documentation for `AoC2024`.
   """
 
   @doc """
@@ -8,11 +8,22 @@ defmodule Aoc2024 do
 
   ## Examples
 
-      iex> Aoc2024.hello()
+      iex> AoC2024.hello()
       :world
 
   """
   def hello do
     :world
+  end
+
+  def parse(input_text, mapper_fn \\ fn {row, _} -> row end) do
+    input_text
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.map(mapper_fn)
+  end
+
+  def load_text(filename) do
+    File.read!(filename)
   end
 end
